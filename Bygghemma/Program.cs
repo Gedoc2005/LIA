8677e66ca9982a24bcf6e905549be208ab86983a
gedoc2005 <gedoc2005@yahoo.com>
2015-11-06 22:11:11 +0100

    Part1

---
+++ Bygghemma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bygghemma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            int j;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                y = i % 3;
+                j = i % 5;
+              
+                if (y == 0)
+                {
+                    Console.Write(" Bygg ");
+                }
+                if (j == 0)
+                {
+                    Console.Write(" Hemma ");
+                }
+                if (y == 0 & j == 0)
+                {
+                    Console.Write(" ByggHemma ");
+                }
+                if(y!=0 & j!=0)
+                {
+                    Console.Write("{0,-3}", i);
+                }
+
+            }
+        }
+    }
+}
