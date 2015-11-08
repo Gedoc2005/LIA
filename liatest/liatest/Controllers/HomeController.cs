8f1ca690222f3ae8ac147cbcba81dadea0bf83dd
gedoc2005 <gedoc2005@yahoo.com>
2015-11-08 22:16:11 +0100

    part3

---
+++ liatest/liatest/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using liatest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace liatest.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private CrimeSceneREST crime = new CrimeSceneREST();
+        public ActionResult CrimeScene()
+        {
+            return View("CrimeScene", crime.GetCrimeScenes());
+        }
+    }
+}
